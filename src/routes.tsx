89339154be582d401617e0e5c9131f652aaf16dd
Treyson <treyson.grange@outlook.com>
2024-07-30 14:08:35 -0600

    Move routes to components folder

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,8 +4,8 @@ import { RouteObject } from 'react-router-dom';
 const Home = React.lazy(() => import('./pages/Home'));
 const Login = React.lazy(() => import('./pages/Login'));
 const Dns = React.lazy(() => import('./pages/Dns'));
-const ProtectedRoute = React.lazy(() => import('./utilities/routes/UserProtectedRoute'));
-const AdminProtectedRoute = React.lazy(() => import('./utilities/routes/AdminProtectedRoute'));
+const ProtectedRoute = React.lazy(() => import('./components/routes/UserProtectedRoute'));
+const AdminProtectedRoute = React.lazy(() => import('./components/routes/AdminProtectedRoute'));
 const AdminTest = React.lazy(() => import('./pages/TestAdmin'));
 
 const routes: RouteObject[] = [
