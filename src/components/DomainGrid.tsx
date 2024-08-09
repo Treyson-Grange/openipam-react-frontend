169535b25c3a4b061ca95d1ad64debbbd0e0dbe2
Treyson <treyson.grange@outlook.com>
2024-08-09 11:57:30 -0600

    allow for pagination when needed

--- src/components/DomainGrid.tsx
+++ src/components/DomainGrid.tsx
@@ -1,38 +1,53 @@
+import { useState, useEffect } from 'react';
 import { usePaginatedApi } from '../hooks/useApi';
 import { useMediaQuery } from '@mantine/hooks';
 import { getApiEndpointFunctions } from '../utilities/apiFunctions';
-import { Card, Text, Title, Grid, Paper, Badge, Group, ActionIcon, em } from '@mantine/core';
+import { Card, Text, Title, Grid, Paper, Badge, Group, ActionIcon, em, Pagination } from '@mantine/core';
 import { FaPencilAlt, FaEye } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 
 const DomainGrid = (): JSX.Element => {
-    const PAGE = 1;
     const PAGE_SIZE = 12;
+    const [page, setPage] = useState(1);
+    const [maxPages, setMaxPages] = useState(0);
 
     const api = getApiEndpointFunctions();
     const { data } = usePaginatedApi(
         api.domain.get,
-        PAGE,
-        PAGE_SIZE,
+        page,
+        PAGE_SIZE
     );
 
-    const calculateSpan = (count: number) => {//not even close to being complete or correct
+    useEffect(() => {
+        if (data?.count) {
+            setMaxPages(Math.ceil(data.count / PAGE_SIZE));
+        }
+    }, [data]);
+
+    const calculateSpan = (count: number) => {//Ignore this for now
         const isMobile = useMediaQuery(`(max-width: ${em(900)})`);
         if (isMobile) {
             return 12;
         }
-        if (count > 12) return 4;
-        if (count > 6) return 3;
-        if (count > 3) return 4;
+
         return 3;
-    }
+    };
 
     const count = data?.count || 0;
     const span = calculateSpan(count);
 
     return (
         <Paper radius='lg' p='lg' m='lg' withBorder>
-            <Title>Your Domains</Title>
+            <Group justify='space-between'>
+                <Title>Your Domains</Title>
+                {maxPages !== 1 && (
+                    <Pagination
+                        total={maxPages}
+                        value={page}
+                        onChange={setPage}
+                    />
+                )}
+            </Group>
             <Grid justify="flex-start" align="stretch" mt="lg">
                 {data?.results?.map((domain: any, index: number) => (
                     <Grid.Col span={span} key={index}>
@@ -74,6 +89,6 @@ const DomainGrid = (): JSX.Element => {
             </Grid>
         </Paper>
     );
-}
+};
 
 export default DomainGrid;
--- src/pages/Domains.tsx
+++ src/pages/Domains.tsx
@@ -2,62 +2,33 @@ import PaginatedTable from '../components/tables/PaginatedTable';
 import DomainGrid from '../components/DomainGrid';
 import { useState } from 'react';
 import { getApiEndpointFunctions } from '../utilities/apiFunctions';
+import { useAuth } from '../contexts/AuthContext';
 
 const Domains = () => {
-    /**
-     * For use in the pagination table. Deletes a DNS record by ID.
-     * @param id 
-     */
-    const deleteDNSRecord: (id: number) => void = (id: number) => {
-        try {
-            api.dns.byId(id).delete().then(() => {
-                console.log('Deleted DNS Record');
-            });
-        } catch (error) {
-            throw new Error();
-        }
-    }
-
-    /**
-     * For use in the pagination table. Logs a host by ID. POC only.
-     * @param id 
-     */
-    const logHost: (id: string) => void = (id: string) => {
-        try {
-            api.host.byId(id).get().then((response) => {
-                console.log(response)
-            });
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
     const [pageSize] = useState<number>(10);
     const api = getApiEndpointFunctions();
-
+    const auth = useAuth();
+    // if (auth.isAdmin()) {
+    //     //paginated table cant handle the links for the domains. fix l8er
+    //     return (
+    //         <>
+    //             <PaginatedTable
+    //                 defPageSize={pageSize}
+    //                 getFunction={api.domain.get}
+    //                 title='Domains'
+    //                 neededAttr={['id', 'name', 'record_count']}
+    //                 morePageSizes={['1']}
+    //                 sortable={true}
+    //                 searchable={true}
+    //                 searchableFields={['name', 'dns_view', 'dns_type']}
+    //             />
+    //         </>
+    //     )
+    // } else {
     return (
-        <>
-            {/* <PaginatedTable
-                defPageSize={pageSize}
-                getFunction={api.domain.get}
-                title="You Domains"
-                neededAttr={['name', 'record_count', 'changed', 'id']}
-                editableObj={true}
-                morePageSizes={['10', '25', '50']}
-                overridePageSizes={true}
-                sortable={true}
-                sortableFields={['name', 'ttl', 'dns_type']}
-                searchable={true}
-                searchableFields={['name', 'content', 'dns_type']}
-                actions={['Delete Selected Dns Records', 'Log Host']}
-                actionFunctions={{
-                    DeleteSelectedDnsRecords: { func: deleteDNSRecord, key: 'id' },
-                    LogHost: { func: logHost, key: 'host' }
-                }}
-            /> */}
-            <DomainGrid />
-        </>
-    );
+        <DomainGrid />
+    )
+    // }
 };
 
 export default Domains;
